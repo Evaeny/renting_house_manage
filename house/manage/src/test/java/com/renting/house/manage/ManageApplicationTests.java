5e6e69e4781109d704eaa69f9420831207117b7e
Evaeny <929497842@qq.com>
2021-12-11 09:44:14 +0800

    初始化项目

---
+++ house/manage/src/test/java/com/renting/house/manage/ManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renting.house.manage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
